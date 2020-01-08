7bed7a6a0a03e8a12052d8ee1d140f33d0b9c1d0
satwik narlanka <satwik.602@gmail.com>
2020-01-08 06:27:02 -0500

    344. Reverse String

---
+++ 344.js
@@ -0,0 +1,16 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let n = s.length;
+    for(let i=0;i<n/2;i++){
+        let temp = s[i];
+        s[i] = s[n-1-i];
+        s[n-1-i] = temp;
+    }
+};
+
+let s = ["h","e","l","l","o"];
+reverseString(s);
+console.log(s);
