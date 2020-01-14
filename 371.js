d4fcdbf881906a86ccfdfeb9078f9c6bd6f15628
satwik narlanka <satwik.602@gmail.com>
2020-01-14 15:05:12 -0500

    371. Sum of Two Integers

---
+++ 371.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    let carry;
+    
+    while(b) {
+        carry = a & b;
+        a ^= b;
+        b = carry << 1;
+    }
+    return a;
+};
+
+console.log(getSum(1,2));
+console.log(getSum(-4,3));
+console.log(getSum(2,-3));
+console.log(getSum(-2,-3));
